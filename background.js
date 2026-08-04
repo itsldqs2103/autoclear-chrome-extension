dfed945fe1ec94612f71a63f1c10cc04105cdef2
itsldqs2103 <itsldqs2103@gmail.com>
2026-08-04 08:05:00 +0700

    chore: initial commit

---
+++ background.js
@@ -0,0 +1,1 @@
+chrome.runtime.onStartup.addListener(()=>chrome.browsingData.remove({},{cache:true,cacheStorage:true,cookies:false,downloads:true,fileSystems:true,formData:true,history:true,indexedDB:true,localStorage:true,passwords:true,serviceWorkers:true}));
