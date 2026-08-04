5fe09e48501d86602f3ec7bcdab145d8efaf5aee
itsldqs2103 <itsldqs2103@gmail.com>
2026-08-04 08:45:01 +0700

    chore: unminify script

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
-chrome.runtime.onStartup.addListener(()=>chrome.browsingData.remove({},{cache:true,cacheStorage:true,cookies:false,downloads:true,fileSystems:true,formData:true,history:true,indexedDB:true,localStorage:true,passwords:true,serviceWorkers:true}));
+chrome.runtime.onStartup.addListener(() =>
+  chrome.browsingData.remove(
+    {},
+    {
+      cache: true,
+      cacheStorage: true,
+      cookies: false,
+      downloads: true,
+      fileSystems: true,
+      formData: true,
+      history: true,
+      indexedDB: true,
+      localStorage: true,
+      passwords: true,
+      serviceWorkers: true,
+    },
+  ),
+);
